4ab6983e581584a1a1a428ceb8ac2b036923e73e
Steve <hello@stephenwillis.co.uk>
2023-02-08 15:17:47 +0000

    Create Algorithms.cs

---
+++ Algorithms.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+class Solution 
+{
+    // Write Function Here
+}
+
+
+/*
+Write a function that returns a count of even numbers.
+
+Here are some example inputs and expected outputs:
+
+1 3 5 7 9 11 –> 0
+
+2 6 8 –> 3
+
+1 2 3 4 –> 2
+*/
+
+
+/*
+Write a function that reverses a string.
+
+Here are some example inputs and expected outputs:
+
+"RSC" –> "RSC"
+
+"abc" –> "cba"
+*/
